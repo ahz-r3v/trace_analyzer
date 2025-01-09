08a93f868b20cc041232506f61a366acedcd0fe7
ahz-r3v <2521920975@qq.com>
2025-01-09 11:41:41 +0800

    v2

--- pkg/info/info.go
+++ pkg/info/info.go
@@ -2,9 +2,9 @@ package info
 
 // InvocationTimestamps holds the timestamps of all invocations for a specific function.
 type FunctionInvocations struct {
-	FunctionName	string    // Function identifier
-	Timestamps   	[]float64 // List of invocation timestamps (in seconds since epoch)
-	Durations     	[]float64 // List of invocation durations (in seconds since epoch)
+	FunctionName    string    // Function identifier
+	Timestamps      []float64 // List of invocation timestamps (in seconds)
+	Durations       []float64 // List of invocation durations (in seconds)
 }
 
 type LabeledTimestamp struct {
